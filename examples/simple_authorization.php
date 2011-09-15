5f4dda9725a248060810b3c09d17c909323f5998
Martins Balodis <maritns256@gmail.com>
2011-09-15 13:04:11 +0300

    Fixed post string

--- examples/simple_authorization.php
+++ examples/simple_authorization.php
@@ -11,19 +11,11 @@ require 'autoload.php';
 
 // create curl object
 $login = new Curl_Browser_GoogleChrome();
-$login->setopt(CURLOPT_COOKIEFILE, 'cookies.txt');
-
-// open page to get a session cookie
-$login->set_fetch_headers();
-$login->exec('http://www.bildites.lv/');
-var_dump($login->get_headers_received());
 
 // set login data
-$login->set_post_string('username:phpmulticurl&password:phpmulticurl&ienakt:Ien%C4%81kt');
+$login->set_post_string('username=phpmulticurl&password=phpmulticurl&ienakt=Ien%C4%81kt');
 // execute login
 $response = $login->exec('http://www.bildites.lv/users.php?act=login-d');
-var_dump($login->get_headers_sent());
-
 
 // check login success
 if(preg_match('/successfully logged in/i', $response)) {
